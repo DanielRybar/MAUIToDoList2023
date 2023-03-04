e8a45345ef14f1a35abe3a7dded847c954b4fddf
Daniel Rybář <danryba019@pslib.cz>
2023-03-05 00:05:09 +0100

    splash

--- MAUIToDoList2023/Views/MainPage.xaml.cs
+++ MAUIToDoList2023/Views/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
-using MAUIToDoList2023.Data;
 using MAUIToDoList2023.Interfaces;
 using MAUIToDoList2023.Models;
 using MAUIToDoList2023.ViewModels;
@@ -9,18 +8,18 @@ namespace MAUIToDoList2023.Views;
 
 public partial class MainPage : ContentPage
 {
-	MainViewModel _vm;
+    MainViewModel _vm;
     EditViewModel _evm;
-	IDataStore<TaskItem> _store;
+    IDataStore<TaskItem> _store;
 
     public MainPage(MainViewModel vm, IDataStore<TaskItem> store, EditViewModel evm)
     {
-		BindingContext = vm;
-		_vm = vm;
-		_store = store;
+        BindingContext = vm;
+        _vm = vm;
+        _store = store;
         _evm = evm;
-		
-		InitializeComponent();
+
+        InitializeComponent();
 
         /*
         MessagingCenter.Subscribe<MainViewModel, string>(this, "removeAll", 
@@ -35,7 +34,7 @@ public partial class MainPage : ContentPage
                 }
             });
         */
-        
+
         WeakReferenceMessenger.Default.Register<ConfirmationMessage>(this, async (r, msg) =>
         {
             bool answer = await DisplayAlert("Potvrzení", msg.Value, "Ano", "Ne");
@@ -50,17 +49,17 @@ public partial class MainPage : ContentPage
     }
 
     private async void OnFinishChecked(object sender, CheckedChangedEventArgs e)
-    {      
+    {
         if (sender is CheckBox box && box.AutomationId != null /*&& _vm.FinishTaskCommand.CanExecute(null)*/)
-		{
+        {
             Debug.WriteLine(box.AutomationId);
             int id = int.Parse(box.AutomationId);
-			var task = await _store.GetItemAsync(id);
+            var task = await _store.GetItemAsync(id);
 
             if (task != null)
-			{
+            {
                 _vm.SelectedTask = task;
-				Debug.WriteLine(_vm.SelectedTask.Title);
+                Debug.WriteLine(_vm.SelectedTask.Title);
                 _vm.FinishTaskCommand.Execute(null);
             }
         }
@@ -69,7 +68,7 @@ public partial class MainPage : ContentPage
     private async void OnAddButtonPressed(object sender, EventArgs e)
     {
         _vm.SelectedTask = null;
-		await Shell.Current.GoToAsync(nameof(AddPage));
+        await Shell.Current.GoToAsync(nameof(AddPage));
     }
 
     private async void TapGestureRecognizer_Highlight(object sender, TappedEventArgs e)
@@ -82,19 +81,20 @@ public partial class MainPage : ContentPage
     {
         Debug.WriteLine("Tapped on: " + _vm.SelectedTask.Title);
         if (sender is Grid grid && grid.AutomationId != null)
+        {
             _vm.SelectedTask = await _store.GetItemAsync(int.Parse(grid.AutomationId));
 
-        _evm.Id = _vm.SelectedTask.TaskId;
-        _evm.TaskTitle = _vm.SelectedTask.Title;
-        _evm.TaskDescription = _vm.SelectedTask.Description;
-        _evm.SelectedDate = _vm.SelectedTask.EndDate;
-        _evm.IsDone = _vm.SelectedTask.IsDone;
-        _evm.SelectedImportance = _vm.SelectedTask.Importance;
-        
+            _evm.Id = _vm.SelectedTask.TaskId;
+            _evm.TaskTitle = _vm.SelectedTask.Title;
+            _evm.TaskDescription = _vm.SelectedTask.Description;
+            _evm.SelectedDate = _vm.SelectedTask.EndDate;
+            _evm.IsDone = _vm.SelectedTask.IsDone;
+            _evm.SelectedImportance = _vm.SelectedTask.Importance;
 
-        await Shell.Current.GoToAsync(nameof(EditPage));
 
-        _vm.SelectedTask = null;
+            await Shell.Current.GoToAsync(nameof(EditPage));
+
+            _vm.SelectedTask = null;
+        }
     }
 }
-
